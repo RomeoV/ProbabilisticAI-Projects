3c3289769f6e25fbe6e5cf593c908fb43b7dadd2
Romeo Valentin <rvalentin@student.ethz.ch>
2020-10-16 12:16:45 +0200

    Implement a more or less working solution

--- project1/just_gpytorch/solution.py
+++ project1/just_gpytorch/solution.py
@@ -1,4 +1,9 @@
 import numpy as np
+import torch
+import gpytorch as gpt
+import matplotlib.pyplot as plt
+
+torch.set_default_tensor_type(torch.DoubleTensor)
 
 ## Constant for Cost function
 THRESHOLD = 0.5
@@ -52,28 +57,79 @@ You can add new methods, and make changes. The checker script performs:
 It uses predictions to compare to the ground truth using the cost_function above.
 """
 
-
-class Model():
-
+class Model:
     def __init__(self):
+        self.BHM: BlazingHotModel
+
+    def fit_model(self, train_x, train_y):
+        self.BHM = BlazingHotModel(train_x, train_y)
+
+    def predict(self, test_x):
+        predictions = self.BHM(test_x).mean.detach()
+        return predictions.numpy()
+
+
+class BlazingHotModel(gpt.models.ExactGP):
+
+    def __init__(self, train_x, train_y):
         """
             TODO: enter your code here
         """
-        pass
+        likelihood = gpt.likelihoods.GaussianLikelihood()
+        super(BlazingHotModel, self).__init__(train_x, train_y, likelihood)
+
+        self.likelihood = likelihood
+        self.mean_module = gpt.means.ConstantMean()
+        self.covar_module = gpt.kernels.ScaleKernel(gpt.kernels.RBFKernel())
+
+        self.fit_model(train_x, train_y, training_iter=20)
+        self.eval()
+        self.likelihood.eval()
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpt.distributions.MultivariateNormal(mean_x, covar_x)
 
     def predict(self, test_x):
         """
             TODO: enter your code here
         """
         ## dummy code below
-        y = np.ones(test_x.shape[0]) * THRESHOLD - 0.00001
-        return y
+        self.eval()
+        self.likelihood.eval()
+        predictions = self.forward(test_x)
+        return predictions.mean.detach()
 
-    def fit_model(self, train_x, train_y):
+    def fit_model(self, train_x, train_y, training_iter=20):
         """
              TODO: enter your code here
         """
-        pass
+        self.train()
+        self.likelihood.train()
+
+        optimizer = torch.optim.Adam([
+            {'params': self.parameters()},
+        ], lr=0.1)
+
+        mll = gpt.mlls.ExactMarginalLogLikelihood(self.likelihood, self)
+
+        for i in range(training_iter):
+            optimizer.zero_grad()
+            output = self.forward(train_x)
+
+            loss = -mll(output, train_y)
+            loss.backward()
+            print(('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f   mean: %.3f' % (
+                i + 1, training_iter, loss.item(),
+                self.covar_module.base_kernel.lengthscale.item(),
+                self.likelihood.noise.item(),
+                self.mean_module.constant.item(),
+                )
+                #+ "   ".join([f"{param} = {val}" for param, val in self.named_parameters()])
+            ))
+
+            optimizer.step()
 
 
 def main():
@@ -87,11 +143,31 @@ def main():
     test_x_name = "test_x.csv"
     test_x = np.loadtxt(test_x_name, delimiter=',')
 
+    # Convert to torch
+    train_x = torch.tensor(train_x).double()
+    train_y = torch.tensor(train_y).double()
+    test_x = torch.tensor(test_x).double()
+
+    N = 10000
+    ind = torch.randperm(train_x.shape[0])
+    train_x = train_x[ind[:N],:]
+    train_y = train_y[ind[:N]]
+    test_x = test_x
+
     M = Model()
     M.fit_model(train_x, train_y)
-    prediction = M.predict(test_x)
 
-    print(prediction)
+    predictions = M.predict(test_x)
+
+    #print(predictions)
+
+    plot_predictions(train_x, train_y, test_x, predictions)
+
+def plot_predictions(train_x, train_y, test_x, pred_y):
+    plt.scatter(train_x[:,0], train_x[:,1], c=train_y, marker='o')
+    plt.scatter(test_x[:,0], test_x[:,1], c=pred_y, marker='x')
+    plt.colorbar()
+    plt.show()
 
 
 if __name__ == "__main__":
